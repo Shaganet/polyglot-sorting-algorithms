4207897d1e40cf019b16b6d3448ff1cb6daba253
Anna <anmax2045@yandex.ru>
2025-09-22 14:43:33 +0300

    Update Bubble_sort.py

--- Bubble_Sort/Python/Bubble_sort.py
+++ Bubble_Sort/Python/Bubble_sort.py
@@ -1,1 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+      for j in range(0,n - i - 1):
+        if arr[j] > arr[j + 1]:
+          arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
+arr = [12234, 61, 1241, 91, 1, 89816]
+sort_arr = bubble_sort(arr) 
+print(f'Sorted array:{sort_arr}')  
